a3a7444680db3786e48027e4f321235cb243cd3f
Allan <allan.ascencio@gmail.com>
2022-09-13 17:34:56 -0300

    Delete answer_state condition

--- main.py
+++ main.py
@@ -23,13 +23,3 @@ while len(guessed_states) < 50:
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
-    # Checking if answer is correct and adding it to the map
-
-    if answer_state in all_states:
-        guessed_states.append(answer_state)
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        state_data = data[data.state == answer_state]
-        t.goto(int(state_data.x), int(state_data.y))
-        t.write(answer_state)
